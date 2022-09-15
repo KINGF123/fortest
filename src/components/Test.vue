4cced2b805a64a0cafcf67428c8b1ac7facd0049
lq <632991814@qq.com>
2022-09-16 03:32:24 +0800

    Changes to be committed: 	modified:   src/components/Test.vue

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -2,6 +2,9 @@
     <div>
         <h3>自定义 {{name}}</h3>
         <button @click="change">按钮</button>
+        <div class="father">
+            <div class="son"></div>
+        </div>
     </div>
 </template>
 
@@ -23,5 +26,20 @@ export default {
 <style lang="less">
     div {
         background-color: cyan;
+    }
+    .father {
+        position: relative;
+        width: 500px;
+        height: 500px;
+        background-color: red;
+        .son {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            width: 40px;
+            height: 40px;
+            background-color: yellow;
+        }
     }
 </style>
