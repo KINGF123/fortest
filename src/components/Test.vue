e990829be685cef974c015d43a3ca2d677a4d807
lq <632991814@qq.com>
2022-09-16 03:19:45 +0800

    Your branch is ahead of 'fortest/master' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/Test.vue

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,5 +1,27 @@
 <template>
     <div>
-        <h3>自定义</h3>
+        <h3>自定义 {{name}}</h3>
+        <button @click="change">按钮</button>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            name:1123
+        }
+    },
+    methods: {
+        change(){
+            this.name = 321
+        }
+    },
+}
+</script>
+
+<style lang="less">
+    div {
+        background-color: cyan;
+    }
+</style>
